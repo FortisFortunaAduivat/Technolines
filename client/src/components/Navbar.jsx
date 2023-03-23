729bc07f9d5540a159c169ee9d8786631db2fa61
13thGhost <fortis7fortuna1adiuvat@gmail.com>
2023-03-23 06:07:02 +0100

    navbar sing in sing up

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,13 +16,30 @@ import { Link as ReactLink } from "react-router-dom";
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { GiAngelWings } from "react-icons/gi";
 
+const links = [
+    { linkName: "Products", link: "/products" },
+    { linkName: "ShoppingCart", link: "/cart" },
+];
+
+const NavLink = ({ path, children }) => (
+    <Link
+        as={ReactLink}
+        to={path}
+        px={2}
+        py={2}
+        rounded="md"
+        _hover={{ textDecoration: "none", bg: useColorModeValue("gray.200", "gray-700") }}
+    >
+        {children}
+    </Link>
+);
 const Navbar = () => {
     const { onOpen, onClose, isOpen } = useDisclosure();
-    const { colorMode, setColorMode } = useColorMode();
+    const { colorMode, setColorMode, toggleColorMode } = useColorMode();
 
     return (
         <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-            <flex h={20} alignItems="center" justifContent="space-between">
+            <Flex h={20} alignItems="center" justifyContent="space-between">
                 <IconButton
                     size="md"
                     icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -39,8 +56,40 @@ const Navbar = () => {
                             </Text>
                         </Flex>
                     </Link>
+                    <HStack>
+                        {links.map((link) => (
+                            <NavLink key={link.linkName} path={link.linkPath}>
+                                {link.linkName}
+                            </NavLink>
+                        ))}
+                    </HStack>
                 </HStack>
-            </flex>
+                <Flex alignItems="center">
+                    <NavLink>
+                        <Icon
+                            as={colorMode === "light" ? MoonIcon : SunIcon}
+                            alignSelf="center"
+                            onClick={() => toggleColorMode()}
+                        />
+                    </NavLink>
+                    <Button as={ReactLink} to="/login" p={2} fontSize="sm" fontWeight={400} variant="link">
+                        Sing-in
+                    </Button>
+                    <Button
+                        as={ReactLink}
+                        to="/registration"
+                        m={2}
+                        fontSize="sm"
+                        fontWeight={700}
+                        _hover={{ bg: "blue.400" }}
+                        bg="blue.500"
+                        color={"white"}
+                    >
+                        Sing-up
+                    </Button>
+                </Flex>
+            </Flex>
+            {isOpen ? <Box pb={4} display={{ md: "none" }}></Box> : null}
         </Box>
     );
 };
